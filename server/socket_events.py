09094d3d4e81ca6cfc796cba362aad716dab9cc3
lavanya1309 <159130578+lavanya1309@users.noreply.github.com>
2025-05-08 14:08:08 +0530

    Update socket_events.py

--- server/socket_events.py
+++ server/socket_events.py
@@ -1,1 +1,10 @@
+from flask_socketio import emit
 
+def register_socket_events(socketio):
+    @socketio.on('connect')
+    def handle_connect():
+        print('Client connected')
+
+    @socketio.on('disconnect')
+    def handle_disconnect():
+        print('Client disconnected')
