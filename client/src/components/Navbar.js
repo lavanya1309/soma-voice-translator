f6a5891e9f8400fa0eba2b9752c5b8cf291c383e
lavanya1309 <159130578+lavanya1309@users.noreply.github.com>
2025-05-08 14:06:51 +0530

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,1 +1,11 @@
+import React from 'react';
 
+const Navbar = () => {
+  return (
+    <nav className="bg-blue-600 p-4 text-white">
+      <h1 className="text-xl font-bold">VideoMeet</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
