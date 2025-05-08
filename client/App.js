cb1427df690301280e802b210ded3a0c3eda6902
lavanya1309 <159130578+lavanya1309@users.noreply.github.com>
2025-05-08 14:11:51 +0530

    Update App.js

--- client/App.js
+++ client/App.js
@@ -1,1 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Navbar from './components/Navbar';
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
