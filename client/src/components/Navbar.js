6681bd2d7e56eae8bcf49600f080e1b0e7c0b314
lavanya1309 <159130578+lavanya1309@users.noreply.github.com>
2025-05-08 14:12:58 +0530

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="bg-blue-600 p-4 text-white">
-      <h1 className="text-xl font-bold">VideoMeet</h1>
+      <div className="container mx-auto flex justify-between">
+        <Link to="/" className="text-xl font-bold">VideoMeet</Link>
+        <div className="space-x-4">
+          <Link to="/login" className="hover:underline">Login</Link>
+          <Link to="/dashboard" className="hover:underline">Dashboard</Link>
+        </div>
+      </div>
     </nav>
   );
 };
