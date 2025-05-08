75e6516f43ec6785b9f255483d785cb07a2fd098
lavanya1309 <159130578+lavanya1309@users.noreply.github.com>
2025-05-08 14:16:46 +0530

    Update socket_events.py

--- server/socket_events.py
+++ server/socket_events.py
@@ -1,10 +1,19 @@
 from flask_socketio import emit
 
 def register_socket_events(socketio):
-    @socketio.on('connect')
-    def handle_connect():
-        print('Client connected')
-
-    @socketio.on('disconnect')
-    def handle_disconnect():
-        print('Client disconnected')
+    @socketio.on('create_room')
+    def handle_create_room(room_id):
+        emit('room_created', {'roomId': room_id})
+    
+    @socketio.on('request_join')
+    def handle_request_join(data):
+        room_id = data['roomId']
+        user_id = request.sid
+        emit('join_requested', {'userId': user_id}, room=room_id)
+    
+    @socketio.on('approve_join')
+    def handle_approve_join(data):
+        emit('join_approved', {
+            'roomId': data['roomId'],
+            'target': data['target']
+        })
